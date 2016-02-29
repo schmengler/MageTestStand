2aab0deb1faac7518f78d917535ec9909509767b
Fabian Schmengler <fs@integer-net.de>
2016-02-29 16:08:12 +0100

    Output coverage as text as well

--- setup.sh
+++ setup.sh
@@ -40,7 +40,7 @@ ${BUILDENV}/install.sh
  
 cd ${BUILDENV}/htdocs
 if [ ! -z $CODECLIMATE_REPO_TOKEN ] ; then
-  ${BUILDENV}/bin/phpunit --colors -d display_errors=1 --coverage-clover build/logs/clover.xml
+  ${BUILDENV}/bin/phpunit --colors -d display_errors=1 --coverage-clover build/logs/clover.xml --coverage-text
   ${BUILDENV}/bin/test-reporter
 else
   ${BUILDENV}/bin/phpunit --colors -d display_errors=1
