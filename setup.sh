ed7fb668a6d9c3e103cf1371bce1fc547f31192d
Fabian Schmengler <fs@integer-net.de>
2016-03-01 12:55:06 +0100

    Run test-reporter in directory where the actual module files used during testing lie

--- setup.sh
+++ setup.sh
@@ -41,7 +41,7 @@ ${BUILDENV}/install.sh
 cd ${BUILDENV}/htdocs
 if [ ! -z $CODECLIMATE_REPO_TOKEN ] ; then
   ${BUILDENV}/bin/phpunit --colors -d display_errors=1 --coverage-clover ${BUILDENV}/clover.xml --coverage-text
-  cd ${WORKSPACE}
+  cd ${BUILDENV}/.modman/`basename ${WORKSPACE}`
   composer require codeclimate/php-test-reporter --dev
   vendor/bin/test-reporter --coverage-report=${BUILDENV}/clover.xml
 else
